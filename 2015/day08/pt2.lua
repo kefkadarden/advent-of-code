f322dce9a3faef69eb52fbd82a1b53ae29da3b38
Kefka D'Arden <kefka.business@gmail.com>
2026-04-27 23:13:19 -0400

    day08 in-progress. pt1 done

---
+++ 2015/day08/pt2.lua
@@ -0,0 +1,37 @@
+if #arg == 0 then
+  print('filename not passed as argument')
+  return
+end
+
+local filename = arg[1]
+
+local function trim(s)
+  if (s == nil) then return nil end
+  return string.gsub(s, "^%s*(.-)%s*$", "%1")
+end
+
+local stotal = 0
+local mtotal = 0
+for line in io.lines(filename) do
+  local scnt = 0
+  local mcnt = 0
+
+  scnt = #line
+
+  local newline
+  line = trim(line)
+
+  newline = string.gsub(line, "\\\\", "\\\\")
+  newline = string.gsub(newline, "\"", "\\\"")
+  --newline = string.gsub(newline, "^\"(.-)\"$", "\"%1\"")
+  newline = string.gsub(newline, "\\x[0123456789abcdef][0123456789abcdef]", "\\A") --Just some random letter to count
+  --newline = string.gsub(newline, "\\\"", "\"")
+  newline = "\"" .. newline .. "\""
+  mcnt = #newline
+
+  stotal = stotal + scnt
+  mtotal = mtotal + mcnt
+  print("line: " .. line .. " newline: " .. newline .. " scnt: " .. scnt .. " mcnt: " .. mcnt)
+end
+
+print(mtotal - stotal)
